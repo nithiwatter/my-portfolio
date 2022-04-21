516a00654308c78df1afbb66053869111b2cad32
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-21 12:36:09 -0400

    add menu for mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import clsx from 'clsx';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
+import type { NextRouter } from 'next/router';
 import { useTheme } from 'next-themes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faBriefcase,
+  faHome,
+  faKeyboard,
+  faMoon,
+  faSun,
+} from '@fortawesome/free-solid-svg-icons';
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import {
+  Menu,
+  MenuList,
+  MenuButton,
+  MenuItem,
+  MenuLink,
+} from '@reach/menu-button';
 
 import { Theme } from '../utils/theme-provider';
 
@@ -14,6 +29,10 @@ type LinkProps = {
   href: string;
   active?: boolean;
   noNextLink?: boolean;
+};
+
+type NavMenuProps = {
+  router: NextRouter;
 };
 
 function Link({ children, href, active, noNextLink }: LinkProps) {
@@ -43,6 +62,52 @@ function Link({ children, href, active, noNextLink }: LinkProps) {
   );
 }
 
+function NavMenu({ router }: NavMenuProps) {
+  return (
+    <Menu>
+      <MenuButton title="Navigation" className="base-button mr-2 sm:hidden">
+        <div className="m-2 flex h-8 w-8 items-center justify-center text-white ">
+          <FontAwesomeIcon icon={faBars} size="lg" />
+        </div>
+      </MenuButton>
+      <MenuList className="menu-list">
+        <MenuItem onSelect={() => router.push('/')}>
+          <FontAwesomeIcon
+            icon={faHome}
+            size="1x"
+            className="mr-2 text-slate-500"
+          />
+          Home
+        </MenuItem>
+        <MenuItem onSelect={() => router.push('/works')}>
+          <FontAwesomeIcon
+            icon={faBriefcase}
+            size="1x"
+            className="mr-2 text-slate-500"
+          />
+          Works
+        </MenuItem>
+        <MenuItem onSelect={() => router.push('/posts')}>
+          <FontAwesomeIcon
+            icon={faKeyboard}
+            size="1x"
+            className="mr-2 text-slate-500"
+          />
+          Posts
+        </MenuItem>
+        <MenuLink as="a" href="https://github.com/nithiwatter/my-portfolio">
+          <FontAwesomeIcon
+            icon={faGithubAlt}
+            size="1x"
+            className="mr-2 text-slate-500"
+          />
+          Source
+        </MenuLink>
+      </MenuList>
+    </Menu>
+  );
+}
+
 function Navbar() {
   const [mounted, setMounted] = React.useState(false);
   const router = useRouter();
@@ -55,8 +120,8 @@ function Navbar() {
 
   return (
     <nav className="fixed top-0 z-10 flex w-full justify-center bg-slate-100/80 backdrop-blur dark:bg-slate-800/80">
-      <div className="flex w-1/2 items-center justify-between p-4">
-        <ul className="flex space-x-8">
+      <div className="flex w-full items-center justify-end p-4 sm:justify-between lg:w-1/2">
+        <ul className="hidden space-x-8 sm:flex">
           <li>
             <Link href="/" active={router.asPath === '/'}>
               Home
@@ -83,6 +148,8 @@ function Navbar() {
             <FontAwesomeIcon icon={faGithubAlt} size="lg" />
           </li>
         </ul>
+
+        <NavMenu router={router} />
 
         <button
           title="Toggle theme"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+// vendors' CSS first to prevent conflicts with tailwind global CSS
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+import '@reach/menu-button/styles.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false;
 
 import Layout from '../components/layout';
 

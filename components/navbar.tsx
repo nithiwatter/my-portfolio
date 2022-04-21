183f4b9d40d8e6324ef77a4eb93c22687796aca5
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-21 08:14:00 -0400

    add stub works and posts

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -51,7 +51,10 @@ function Navbar() {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/">Works</Link>
+            <Link href="/works">Works</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
           </li>
           <li className="flex items-end space-x-2">
             <Link href="https://github.com/nithiwatter/my-portfolio" noNextLink>
---
+++ pages/works.tsx
@@ -0,0 +1,5 @@
+function Work() {
+  return <div>Work</div>;
+}
+
+export default Work;

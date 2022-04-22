6c66af59728ed5760a78327da933873bbc57fa78
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-22 12:55:01 -0400

    finish animation

--- components/modelLoader.tsx
+++ components/modelLoader.tsx
@@ -1,9 +1,21 @@
-import React, { Suspense } from 'react';
+import React, { CSSProperties } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Loader } from '@react-three/drei';
 
 import CanvasWrapper from './canvasWrapper';
 import { useCheckMounted } from '../utils/hooks';
+
+const styles: { [key: string]: CSSProperties } = {
+  container: {
+    width: 200,
+    height: 100,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '0.375rem',
+    background: 'rgba(23, 23, 23, 0.5)',
+  },
+};
 
 function ModelLoader() {
   // const cameraRef = React.useRef<THREE.PerspectiveCamera>();
@@ -17,7 +29,7 @@ function ModelLoader() {
       <Canvas shadows>
         <CanvasWrapper />
       </Canvas>
-      <Loader />
+      <Loader containerStyles={styles.container} innerStyles={styles.inner} />
     </>
   );
 }

60820b0793a5c5174ac6e9b30f7e398a419d207f
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-22 07:09:23 -0400

    configure react three installation

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+const withTM = require('next-transpile-modules')(['three']);
+module.exports = withTM(nextConfig);

d1e74c182a299eab36d7fc750cffafe5dc372c4a
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2022-04-25 13:07:54 -0400

    add more experiences

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 import type { StaticImageData } from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 import { AnimatedOnEnter } from '../components/animation';
 
 import dukeLogo from '../public/images/duke.png';
 import reactLogo from '../public/images/react.png';
 import pythonLogo from '../public/images/python.png';
+import innovationStudioLogo from '../public/images/innovation-studio.png';
+import tencentLogo from '../public/images/tencent.png';
 import myteapal from '../public/images/myteapal.png';
+import hours from '../public/images/hours.webp';
 
 const shortExperiences = [
   {
@@ -36,12 +41,73 @@ const shortExperiences = [
   },
 ];
 
+const secondShortExperiences = [
+  {
+    title: 'Duke Innovation Studio',
+    date: 'Nov 2020',
+    content:
+      'Worked as a startup engineer at Duke Innovation Studio, an incubator. Had fun experimenting more with Firebase and React and working with data scientists and designers.',
+    src: innovationStudioLogo,
+    href: 'https://www.dukeinnovationstudio.com/',
+    tags: ['Firebase', 'Python', 'React', 'Redux'],
+  },
+  {
+    title: 'Machine Learning Engineer Intern',
+    date: 'Jun 2021',
+    content:
+      'Interned at Tencent. Self studied NLP to develop deep networks recommeding news and songs. Built pipelines to automate web crawling news and songs data and train transformers and deep convolutional networks across multiple GPUs. Adopted as internal prototypes for new features.',
+    src: tencentLogo,
+    href: 'https://www.tencent.co.th/en/',
+    tags: ['Python', 'Pytorch', 'Scrapy', 'Selenium'],
+  },
+];
+
+const experiencePanes = [
+  {
+    title: 'Software Engineer for MyTeaPal',
+    date: 'Oct 2020',
+    starter: 'First time building something for actual users! ',
+    content:
+      'Cold emailed asking the founder to help work on the app. Learned Firebase and Flutter and shipped the first major update within the first week. Shipped over 7 major updates, increasing the user base from 500 to 5,000 within 5 months. Built new social network features and a pipeline for localization and caching.',
+    src: myteapal,
+    href: 'https://myteapal.com/',
+    tags: ['Firebase', 'Flutter'],
+    showFirstTag: true,
+  },
+];
+
+const secondExperiencePanes = [
+  {
+    title: 'Software Engineer for Hours By Fiveable',
+    date: 'July 2021',
+    starter: undefined,
+    content:
+      'Worked for Hours by Fiveable, a virtual studying platform. Spearheaded refactoring efforts for both frontend and backend codebases, lowering the socket server load by over 90% and bundle size by over 30%. Improved Lighthouse overall metrics by over 20 points.',
+    src: hours,
+    href: 'https://fiveable.me/',
+    tags: ['Express', 'Node.js', 'React', 'Redux', 'Socket.IO'],
+    showFirstTag: true,
+  },
+];
+
 type ShortExperiencePaneProps = {
   title: string;
   date: string;
   content: string;
   src: StaticImageData;
   href: string;
+  tags?: string[];
+};
+
+type ExperiencePaneProps = {
+  title: string;
+  date: string;
+  starter?: string;
+  content: string;
+  src: StaticImageData;
+  href: string;
+  tags?: string[];
+  showFirstTag?: boolean;
 };
 
 function WorksHeroSection() {
@@ -78,6 +144,7 @@ function ShortExperiencePane({
   content,
   src,
   href,
+  tags,
 }: ShortExperiencePaneProps) {
   return (
     <div className="flex justify-center text-white">
@@ -85,7 +152,7 @@ function ShortExperiencePane({
         href={href}
         target="_blank"
         rel="noreferrer"
-        className="color-text w-full rounded-md bg-blue-500 p-4 transition-colors hover:bg-blue-400 focus:bg-blue-400 focus:ring-2 focus:ring-offset-2 xl:w-1/2"
+        className="color-text w-full rounded-md bg-blue-500 p-4 shadow-md transition-colors hover:bg-blue-400 focus:bg-blue-400 focus:ring-2 focus:ring-offset-2 xl:w-1/2"
       >
         <div className="flex items-center">
           <div>
@@ -103,14 +170,27 @@ function ShortExperiencePane({
             </div>
           </div>
 
-          <div className="grow">
-            <div className="mb-2 flex flex-col items-start justify-between sm:flex-row sm:items-center">
+          <div className="grow space-y-2">
+            <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
               <h3 className="color-text mb-1 sm:mb-0">{title}</h3>
               <div className="rounded-md bg-gray-200 py-1 px-2 text-blue-600">
                 <span>{date}</span>
               </div>
             </div>
             <p className="color-text">{content}</p>
+
+            {tags && (
+              <div className="flex flex-wrap gap-2">
+                {tags.map((tag) => (
+                  <div
+                    key={tag}
+                    className="inline-block rounded-md bg-gray-200 py-1 px-2 text-blue-600"
+                  >
+                    <span>{tag}</span>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </a>
@@ -118,17 +198,26 @@ function ShortExperiencePane({
   );
 }
 
-function ExperiencePane() {
+function ExperiencePane({
+  title,
+  date,
+  starter,
+  content,
+  src,
+  href,
+  tags,
+  showFirstTag,
+}: ExperiencePaneProps) {
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="order-1 mb-8 w-full grow md:order-none md:w-1/3">
+      <div className="order-1 w-full grow md:order-none md:mb-8 md:w-1/3">
         <a
-          href="#"
-          className="relative block aspect-[4/3] w-full overflow-hidden rounded-2xl shadow-md transition-transform duration-300 hover:-translate-y-3  focus:ring-2 focus:ring-offset-2"
+          href={href}
+          className="relative block aspect-[4/3] w-full overflow-hidden rounded-2xl shadow-md transition-transform duration-300 hover:-translate-y-3 focus:ring-2 focus:ring-offset-2"
         >
           <Image
-            src={myteapal}
-            alt="myteapal"
+            src={src}
+            alt={title}
             layout="fill"
             objectFit="cover"
             objectPosition="center center"
@@ -138,24 +227,50 @@ function ExperiencePane() {
       </div>
 
       <div className="mx-4 mb-4 flex flex-col items-center md:mb-0">
-        <span className="timeline h-10 border-r-2 md:hidden" />
+        <span className="timeline h-12 border-r-2 md:hidden" />
         <span className="timeline h-2 w-2 rounded-full border-2" />
         <span className="timeline hidden grow border-r-2 md:block" />
       </div>
 
-      <div className="mb-8 w-full grow self-center text-center md:mb-0 md:w-1/2 md:self-auto md:text-left">
-        <h2>Software Engineer for MyTeaPal</h2>
+      <div className="mb-8 w-full grow space-y-2 self-center text-center md:w-1/2 md:self-auto md:text-left">
+        <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
+          <h2 className="mr-2">{title}</h2>
+          <div className="rounded-md bg-gray-200 py-1 px-2 text-blue-600">
+            <span>{date}</span>
+          </div>
+        </div>
+
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          asds five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          asdas including versions of Lorem Ipsum.
+          {starter && <span className="font-bold">{starter}</span>}
+          {content}
         </p>
+
+        <div className="flex flex-wrap justify-center gap-2 md:justify-start">
+          {showFirstTag && (
+            <div className="inline-block rounded-md bg-gray-200 py-1 px-2 text-blue-600">
+              <FontAwesomeIcon
+                icon={faArrowDown}
+                size="lg"
+                className="mr-2 md:hidden"
+              />
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                size="lg"
+                className="mr-2 hidden md:inline-block"
+              />
+              <span>Check it out!</span>
+            </div>
+          )}
+
+          {tags?.map((tag) => (
+            <div
+              key={tag}
+              className="inline-block rounded-md bg-gray-200 py-1 px-2 text-blue-600"
+            >
+              <span>{tag}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -181,8 +296,78 @@ function Work() {
                 />
               ))}
             </div>
-            <ExperiencePane />
-            <ExperiencePane />
+
+            <div>
+              {experiencePanes.map(
+                ({
+                  title,
+                  date,
+                  starter,
+                  content,
+                  src,
+                  href,
+                  tags,
+                  showFirstTag,
+                }) => (
+                  <ExperiencePane
+                    key={title}
+                    title={title}
+                    date={date}
+                    starter={starter}
+                    content={content}
+                    src={src}
+                    href={href}
+                    tags={tags}
+                    showFirstTag={showFirstTag}
+                  />
+                )
+              )}
+            </div>
+
+            <div className="space-y-4">
+              {secondShortExperiences.map(
+                ({ title, date, content, src, href, tags }) => (
+                  <ShortExperiencePane
+                    key={title}
+                    title={title}
+                    date={date}
+                    content={content}
+                    src={src}
+                    href={href}
+                    tags={tags}
+                  />
+                )
+              )}
+            </div>
+
+            <div>
+              {secondExperiencePanes.map(
+                ({
+                  title,
+                  date,
+                  starter,
+                  content,
+                  src,
+                  href,
+                  tags,
+                  showFirstTag,
+                }) => (
+                  <ExperiencePane
+                    key={title}
+                    title={title}
+                    date={date}
+                    starter={starter}
+                    content={content}
+                    src={src}
+                    href={href}
+                    tags={tags}
+                    showFirstTag={showFirstTag}
+                  />
+                )
+              )}
+            </div>
+
+            <div>Footer</div>
           </div>
         </div>
       </AnimatedOnEnter>
